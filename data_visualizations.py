29b41094964ae6c02034920f57f05ff9e9826f9b
ghappy112 <65770357+ghappy112@users.noreply.github.com>
2021-03-04 19:46:02 -0600

    Create data_visualizations.py

---
+++ data_visualizations.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load data
+df = pd.read_csv(r"C:\Users\Greg Happ\Desktop\DATA INCUBATOR\capstone_car_accident_forecasting\US_Accidents_June20.csv", usecols=["Severity", "Temperature(F)", "Wind_Chill(F)", "Pressure(in)", "Visibility(mi)", "Wind_Speed(mph)", "Precipitation(in)", "Sunrise_Sunset"])
+
+#########################################################################################
+# set theme
+sns.set_theme(style="ticks")
+
+# scatter plot matrix
+sns.pairplot(df.sample(frac=0.001, random_state=42), hue="Severity")
+plt.show()
+#########################################################################################
+
+#########################################################################################
+# reset theme
+sns.set_theme(style="darkgrid")
+
+# bar chart
+sns.countplot(x="Severity", data=df)
+plt.show()
+##########################################################################################
