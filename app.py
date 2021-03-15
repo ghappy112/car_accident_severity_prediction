7733fbbd4f2fc055d037d7a074d7968074bc8b16
ghappy112 <65770357+ghappy112@users.noreply.github.com>
2021-03-15 14:42:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,57 +54,68 @@ def home():
     
 @app.route('/predictor', methods=['GET', 'POST'])
 def predictor():
-
-    #get user input
-    data = request.form
-    city = data.get('city')
-    state = data.get('state')
-    country = data.get('country')
-    state = state.upper()
-    country = country.upper()
-    if country == "USA" or country == "U.S." or country == "U.S.A.":
-        country = "US"
-
-    #get weather data from weatherbit
-    Key = "You can create a key for free at weatherbit.io. If you want my key, please message me on slack."
-    url = "https://api.weatherbit.io/v2.0/current?city=" + city + "&state=" + state + "&country=" + country + "&key=" + Key + "&include=minutely"
-    r = requests.get(url, auth=('user', 'pass'))
-    weather_data = r.json()
-    temp = weather_data['data'][0]['temp']
-    temp = (temp * 9/5) + 32
-    wind_spd = weather_data['data'][0]['wind_spd']
-    wind_spd *= 2.23694
-    wind_chill = temp - (wind_spd * 0.7)
-    pres = weather_data['data'][0]['pres']
-    pres *= 0.0295301
-    vis = weather_data['data'][0]['vis']
-    vis *= 0.621371
-    precip = weather_data['data'][0]['precip']
-    precip *= 0.0394 * 24
-    pod = weather_data['data'][0]['pod']
-    if pod == 'd':
-        day = 1
-        night = 0
-    elif pod == 'n':
-        day = 0
-        night = 1
-    X = [temp, wind_chill, pres, vis, wind_spd, precip, day, night]
     
-    #ml prediction
-    prediction = (model.predict([X]))[0] 
-    prediction = str(prediction)
-    
-    return '''
-        <html>
-            <body style="background-color:CornflowerBlue;">
-                <h1 style="color:white;">Car Accident Severity Prediction:</h1>
-                <h1 style="color:white;">''' + prediction + '''</h1>
-                <br>
-                <h1 style="color:white;">(Traffic delay severity estimates range between 1-4. 4 is more severe than 3, 3 is more severe than 2, and 2 is more severe than 1)</h1>
-                <h1 style="color:white;">(Severity is defined as how long traffic delays caused by the accident are)</h1>
-            </body>
-        </html>
-    '''
+    gohome = 0
+    try:
+        #get user input
+        data = request.form
+        city = data.get('city')
+        state = data.get('state')
+        country = data.get('country')
+        state = state.upper()
+        country = country.upper()
+        if country == "USA" or country == "U.S." or country == "U.S.A.":
+            country = "US"
+
+        #get weather data from weatherbit
+        Key = "You can create a key for free at weatherbit.io. If you want my key, please message me on slack."
+        url = "https://api.weatherbit.io/v2.0/current?city=" + city + "&state=" + state + "&country=" + country + "&key=" + Key + "&include=minutely"
+        r = requests.get(url, auth=('user', 'pass'))
+        weather_data = r.json()
+        temp = weather_data['data'][0]['temp']
+        temp = (temp * 9/5) + 32
+        wind_spd = weather_data['data'][0]['wind_spd']
+        wind_spd *= 2.23694
+        wind_chill = temp - (wind_spd * 0.7)
+        pres = weather_data['data'][0]['pres']
+        pres *= 0.0295301
+        vis = weather_data['data'][0]['vis']
+        vis *= 0.621371
+        precip = weather_data['data'][0]['precip']
+        precip *= 0.0394 * 24
+        pod = weather_data['data'][0]['pod']
+        if pod == 'd':
+            day = 1
+            night = 0
+        elif pod == 'n':
+            day = 0
+            night = 1
+        X = [temp, wind_chill, pres, vis, wind_spd, precip, day, night]
+
+        #ml prediction
+        prediction = (model.predict([X]))[0] 
+        prediction = str(prediction)
+
+        return '''
+            <html>
+                <body style="background-color:CornflowerBlue;">
+                    <h1 style="color:white;">Car Accident Severity Prediction:</h1>
+                    <h1 style="color:white;">''' + prediction + '''</h1>
+                    <br>
+                    <h1 style="color:white;">(Traffic delay severity estimates range between 1-4. 4 is more severe than 3, 3 is more severe than 2, and 2 is more severe than 1)</h1>
+                    <h1 style="color:white;">(Severity is defined as how long traffic delays caused by the accident are)</h1>
+                </body>
+            </html>
+        '''
+    except Exception as et:
+        gohome = et
+        return '''
+            <html>
+                <body>
+                    <h1>''' + str(gohome) + '''</h1>
+                </body>
+            </html>
+        '''
 
 if "__name__" == "__main__":
     app.run(debug=True)
